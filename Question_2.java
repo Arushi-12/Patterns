bd2305595a9f622e4bf9a46712fafeb258cfc42a
Arushi-12 <118588760+Arushi-12@users.noreply.github.com>
2023-04-25 22:14:06 +0530

    q_2

---
+++ Question_2.java
@@ -0,0 +1,23 @@
+// To print the following pattern
+// *****
+// *   *
+// *   *
+// *****
+
+
+public class Pattern{
+    public static void main(String[] args){
+        int n = 4;
+        int m = 5;
+        for(int i =1;i<=n;i++){
+            for(int j = 1;j<=m;j++) {
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
